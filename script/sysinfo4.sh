f1224e8213181cf7e551107bf46d8f6475fc34f9
Marco <marco.lovazzano@gmail.com>
2018-09-01 12:08:53 +0200

    Table output, ram usage, uptime and load average output

--- script/sysinfo4.sh
+++ script/sysinfo4.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #--------------------------------------------------------------------------------------------------
-# Stats4shell script
+# Sysinfo4shell script
 # Copyright (c) Marco Lovazzano
 # Licensed under the GNU General Public License v3.0
 # http://github.com/martcus
@@ -28,7 +28,7 @@ __base="$(basename ${__file} .sh)"
 __root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
 
 # log4.sh inclusion
-source ../../log4sh/script/log4.sh -v DEBUG # -d "+%Y-%m-%d %H:%M:%S" # use -f $__file for log
+source ../../log4sh/script/log4.sh -v INFO # -d "+%Y-%m-%d %H:%M:%S" # use -f $__file for log
 
 ## Clear the screen
 clear
@@ -38,13 +38,56 @@ DEBUG "__file = "$__file
 DEBUG "__base = "$__base
 DEBUG "__root = "$__root
 
+tempfile=.sysinfolog
+
+## Utils
+newline() {
+  echo "|"
+}
+
 ## OS info
 
 ## RAM usage
-
+_ramusage() {
+ free -h | grep -v + >> .sysinfologtemp 
+ # header
+ echo -e "Memory Usages| Total|Used |Free" >> $tempfile
+ cat .sysinfologtemp | grep "Mem" | awk '{print " Ram | "$2 "|" $3 "|" $4}' >> $tempfile
+ #echo -e "Swap Usages| Total|Used |Free" >> $tempfile
+ cat .sysinfologtemp | grep "Swap" | awk '{print " Swap | "$2 "|" $3 "|" $4}' >> $tempfile
+ rm .sysinfologtemp
+ newline >> $tempfile
+}
 ## File System
 
 ## Check System Uptime
+_uptime() {
+  uptime=$(uptime | awk '{print $3,$4}' | cut -f1 -d,)
+  echo "System Uptime Days/(HH:MM)| "$uptime >> $tempfile
+  newline >> $tempfile
+}
+
+## Load average
+_loadaverage() {
+  loadaverage=$(top -n 1 -b | grep "load average:" | awk '{print $11 $12 $13}')
+  echo -e "Load Average|" $loadaverage >> $tempfile
+  newline >> $tempfile
+}
+
+## Main
+# retrieve all info
+_uptime
+
+_loadaverage
+
+_ramusage
+
+# Print result in table format
+column -t -s '|' $tempfile
+
+# remove temporary file
+DEBUG "Remove temp file '$tempfile'"
+rm $tempfile
 
 # Restore IFS
 IFS=$SAVEIFS
